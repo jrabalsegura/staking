2e19b3c788ca97dcd7f926f8d8257f59e006c546
JoseRabal <jrabal.segura@gmail.com>
2024-09-17 17:20:16 +0200

    Running collectstatic in the Dockerfile!

--- stakes_manager/stakes_manager/settings.py
+++ stakes_manager/stakes_manager/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'stakes/static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
