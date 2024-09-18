16bc6bca05d3fe3aad7388766e1514e5292d03d8
JoseRabal <jrabal.segura@gmail.com>
2024-09-18 09:15:58 +0200

    Change path from /stake to '' and adding viewport to check if it is better in mobile

--- stakes_manager/stakes_manager/settings.py
+++ stakes_manager/stakes_manager/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-
+#SECRET_KEY = 'django-insecure-1234567890'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
--- stakes_manager/stakes_manager/urls.py
+++ stakes_manager/stakes_manager/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('stake/', include('stakes.urls')),
+    path('', include('stakes.urls')),
 ]
 
 
