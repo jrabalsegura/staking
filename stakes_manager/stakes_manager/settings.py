d008fd0db6b083d21437d847afbf7bf91641928c
JoseRabal <jrabal.segura@gmail.com>
2024-09-19 10:37:14 +0200

    Fixed problem in Settings, reconfigured for production

--- stakes_manager/stakes_manager/settings.py
+++ stakes_manager/stakes_manager/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-SECRET_KEY = 'django-insecure-1234567890'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-1234567890')
+#SECRET_KEY = 'django-insecure-1234567890'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('IS_DEVELOPMENT', True)
 
