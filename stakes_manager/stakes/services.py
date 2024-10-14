9e8d69234bf7250605eabe365dc0447cfdedb7bd
JoseRabal <jrabal.segura@gmail.com>
2024-10-14 21:35:41 +0200

    refactor and added services layer for process_bet

---
+++ stakes_manager/stakes/services.py
@@ -0,0 +1,82 @@
+from .models import Bet, BetPending
+from datetime import datetime
+
+
+min_stake = 0.01
+max_stake = 0.07
+multiplier_y = 0.002
+multiplier_n = 0.002544
+#OJO cambiar también en api!!!! 
+
+def round_to_5_decimals(number):
+    return round(number, 5)
+
+def check_if_same_day(date1, date2):
+    return date1.day == date2.day and date1.month == date2.month and date1.year == date2.year
+
+def process_bet(stake, odd, choice):
+    last_bet = Bet.objects.last()
+            
+    multiplier = last_bet.next_multiplier
+    balance = last_bet.balance
+    last_bet_date = last_bet.created_at
+    last_daily_profit = last_bet.daily_profit
+    same_day = check_if_same_day(last_bet_date, datetime.now())
+    
+    if choice == 'y':
+        balance = round_to_5_decimals(balance + stake * odd - stake)
+        next_multiplier = round_to_5_decimals(max(multiplier - multiplier_y, min_stake))
+        next_stake = round_to_5_decimals(next_multiplier * balance)
+        if same_day:
+            daily_profit = round_to_5_decimals(last_daily_profit + stake * odd - stake)
+        else:
+            daily_profit = round_to_5_decimals(stake * odd - stake)
+        
+    elif choice == 'n':
+        balance = round_to_5_decimals(balance - stake)
+        next_multiplier = round_to_5_decimals(min(multiplier + multiplier_n, max_stake))
+        next_stake = round_to_5_decimals(next_multiplier * balance)
+        if same_day:
+            daily_profit = round_to_5_decimals(last_daily_profit - stake)
+        else:
+            daily_profit = round_to_5_decimals(-stake)
+    elif choice == 'hl':
+        balance = round_to_5_decimals(balance - stake / 2)
+        next_multiplier = round_to_5_decimals(min(multiplier + multiplier_n, max_stake))
+        next_stake = round_to_5_decimals(next_multiplier * balance)
+        if same_day:
+            daily_profit = round_to_5_decimals(last_daily_profit - stake / 2)
+        else:
+            daily_profit = round_to_5_decimals(-stake / 2)
+    
+    # Create a new Bet
+    Bet.objects.create(
+        stake=stake,
+        odd=odd,
+        result=choice,
+        next_multiplier=next_multiplier,
+        balance=balance,  # You may want to calculate this based on your logic
+        next_stake=next_stake,
+        daily_profit=daily_profit
+    )
+    
+def get_last_bet():
+    return Bet.objects.last()
+
+def get_last_10_bets():
+    return Bet.objects.order_by('-created_at')[:10]
+
+def get_pending_bets():
+    return BetPending.objects.all()
+
+def create_bet_pending(stake, odd):
+    BetPending.objects.create(
+        stake=stake,
+        odd=odd
+    )
+    
+def get_last_stake():
+    last_bet = get_last_bet()
+    return last_bet.next_stake if last_bet else 0
+
+
--- stakes_manager/stakes/views.py
+++ stakes_manager/stakes/views.py
@@ -4,63 +4,7 @@ from django.urls import reverse
 from .models import Bet, BetPending
 import math
 from datetime import datetime
-min_stake = 0.01
-max_stake = 0.07
-multiplier_y = 0.002
-multiplier_n = 0.002544
-#OJO cambiar también en api!!!! 
-
-def round_to_5_decimals(number):
-    return round(number, 5)
-
-def check_if_same_day(date1, date2):
-    return date1.day == date2.day and date1.month == date2.month and date1.year == date2.year
-
-def process_bet(stake, odd, choice):
-    last_bet = Bet.objects.last()
-            
-    multiplier = last_bet.next_multiplier
-    balance = last_bet.balance
-    last_bet_date = last_bet.created_at
-    last_daily_profit = last_bet.daily_profit
-    same_day = check_if_same_day(last_bet_date, datetime.now())
-    
-    if choice == 'y':
-        balance = round_to_5_decimals(balance + stake * odd - stake)
-        next_multiplier = round_to_5_decimals(max(multiplier - multiplier_y, min_stake))
-        next_stake = round_to_5_decimals(next_multiplier * balance)
-        if same_day:
-            daily_profit = round_to_5_decimals(last_daily_profit + stake * odd - stake)
-        else:
-            daily_profit = round_to_5_decimals(stake * odd - stake)
-        
-    elif choice == 'n':
-        balance = round_to_5_decimals(balance - stake)
-        next_multiplier = round_to_5_decimals(min(multiplier + multiplier_n, max_stake))
-        next_stake = round_to_5_decimals(next_multiplier * balance)
-        if same_day:
-            daily_profit = round_to_5_decimals(last_daily_profit - stake)
-        else:
-            daily_profit = round_to_5_decimals(-stake)
-    elif choice == 'hl':
-        balance = round_to_5_decimals(balance - stake / 2)
-        next_multiplier = round_to_5_decimals(min(multiplier + multiplier_n, max_stake))
-        next_stake = round_to_5_decimals(next_multiplier * balance)
-        if same_day:
-            daily_profit = round_to_5_decimals(last_daily_profit - stake / 2)
-        else:
-            daily_profit = round_to_5_decimals(-stake / 2)
-    
-    # Create a new Bet
-    Bet.objects.create(
-        stake=stake,
-        odd=odd,
-        result=choice,
-        next_multiplier=next_multiplier,
-        balance=balance,  # You may want to calculate this based on your logic
-        next_stake=next_stake,
-        daily_profit=daily_profit
-    )
+from .services import process_bet, check_if_same_day, get_last_bet, get_last_10_bets, get_pending_bets, create_bet_pending
 
 # Create your views here.
 @require_http_methods(["GET", "POST"])
@@ -74,15 +18,14 @@ def stake_view(request):
             process_bet(stake, odd, choice)
         else:
             # Create a new BetPending
-            BetPending.objects.create(
-                stake=stake,
-                odd=odd,
-            )
+            create_bet_pending(stake, odd)
         
         return redirect('stakes:stake')  # Redirect back to the same page after processing
 
+    #Seguro que esto se podría refactorizar más para quitar toda la lógica de negocio de la vista
+    #pero bueno, funciona.
     # Get the last bet for stake and multiplier
-    last_bet = Bet.objects.last()
+    last_bet = get_last_bet()
     current_stake = last_bet.next_stake if last_bet else 0
     current_multiplier = last_bet.next_multiplier if last_bet else 0
     
@@ -93,10 +36,10 @@ def stake_view(request):
         daily_profit = 0
 
     # Get pending bets
-    pending_bets = BetPending.objects.all()
+    pending_bets = get_pending_bets()
 
     # Get last 10 bets in reverse order
-    last_bets = Bet.objects.order_by('-created_at')[:10]
+    last_bets = get_last_10_bets()
 
     context = {
         'current_stake': current_stake,
